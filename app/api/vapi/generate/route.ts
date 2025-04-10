dec135eeb3b136f4c160acabceba50f3a94cb563
Arego11 <aregar20012001@gmail.com>
2025-04-10 16:26:58 -0700

    generates the questions!

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -1,5 +1,7 @@
 import { generateText } from "ai"
-import {google} from "@ai-sdk/google"
+import { google } from "@ai-sdk/google"
+import { getRandomInterviewCover } from "@/lib/utils"
+import { db } from "@/firebase/admin"
 
 export async function GET() {
     return Response.json({ success: true, data: 'THANK YOU!' }, { status: 200 })
@@ -9,7 +11,7 @@ export async function POST(request: Request) {
     const { type, role, level, techstack, amount, userid } = await request.json()
 
     try {
-        const { text } = await generateText({
+        const { text: questions } = await generateText({
             model: google('gemini-2.0-flash-001'),
             prompt: `Prepare questions for a job interview.
         The job role is ${role}.
@@ -25,9 +27,37 @@ export async function POST(request: Request) {
         Thank you! <3`,
         })
 
+        const interview = {
+            role, type, level,
+            techstack: techstack.split(','),
+            questions: JSON.parse(questions),
+            userId: userid,
+            finalized: false,
+            coverImage: getRandomInterviewCover,
+            createdAt: new Date().toISOString()
+        }
+
+        await db.collection('interviews').add(interview)
+
+        return Response.json({ success: true }, { status: 200 })
+
     } catch (error) {
         console.error(error)
 
         return Response.json({ success: false, error }, {status: 500})
     }
 }
+
+{/*
+    sign in information
+
+    {
+    "type": "mixed",
+    "role": "frontend",
+    "level": "senior",
+    "techstack": "next.js",
+    "amount": "7",
+    "userid":
+    "PJ5IarwrsBgybZAFnDpbi02hbL62" -> your userID
+}
+    */}
