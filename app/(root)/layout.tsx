8188ccf33934d560a9ba77a331f9f3896d99f8c7
Arego11 <aregar20012001@gmail.com>
2025-04-13 20:10:36 -0700

    not redirecting when signed in

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import { ReactNode } from "react";
-import { redirect } from "next/navigation";
+//import { redirect } from "next/navigation";
 
-import { isAuthenticated } from "@/lib/actions/auth.action";
+//import { isAuthenticated } from "@/lib/actions/auth.action";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
-  const isUserAuthenticated = await isAuthenticated();
+  //const isUserAuthenticated = await isAuthenticated();
   //if (!isUserAuthenticated) redirect("/sign-in");
 
   return (
