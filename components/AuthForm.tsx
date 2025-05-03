6877af86aab90ed7271a22ceb96e3b2fcf9ab70d
Arego11 <aregar20012001@gmail.com>
2025-05-02 18:42:13 -0700

    small changed

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -88,7 +88,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
           <Image src="/logo.svg" alt="logo" width={38} height={32} />
-          <h2 className="text-primary-100"> PrepWise</h2>
+          <h2 className="text-primary-100"> Interviews</h2>
         </div>
 
         <h3> Practice job interview with AI</h3>
